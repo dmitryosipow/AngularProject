58862fb830a39a0533ee904e884d163b2084c801
Dmitry Osipov <dmitryosipow@gmail.com>
2014-07-23 02:42:43 +0300

    Init

---
+++ app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularProjectApp
+ * @description
+ * # angularProjectApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularProjectApp', [
+    'ngAnimate'
+  ]);
