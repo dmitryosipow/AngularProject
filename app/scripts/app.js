14635ec7a52ead9adec9340a6f759f7b3f2651d3
Dmitry Osipov <dmitryosipow@gmail.com>
2014-07-29 17:12:15 +0300

    Proceed

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,7 +8,6 @@
  *
  * Main module of the application.
  */
-var a = 5;
-console.log('12345');
-angular
+
+var app = angular
   .module('angularProjectApp', ['ngSanitize']);
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,17 +7,48 @@
  * # MainCtrl
  * Controller of the angularProjectApp
  */
-angular.module('angularProjectApp')
-  .controller('MainCtrl', function ($scope, $http) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-console.log('fdsfds');
-    var store = this;
-    store.posts = [];
-    $http.get('posts.json').success(function(data){
-      store.posts = data;
-    });
+app.controller('MainCtrl', function($scope, $http) {
+
+
+  $scope.modalShown = false;
+  $scope.toggleModal = function() {
+    $scope.modalShown = !$scope.modalShown;
+  };
+
+  var store = this;
+  store.posts = [];
+  $http.get('posts.json').success(function(data) {
+    store.posts = data;
   });
+
+  this.post = {};
+
+  this.addPost = function() {
+    this.post.date = {};
+    this.post.date.$date = Date.now();
+    store.posts.push(this.post);
+    this.post = {};
+  };
+});
+
+app.directive('modalDialog', function() {
+  return {
+    restrict: 'E',
+    scope: {
+      show: '='
+    },
+    transclude: true, // we want to insert custom content inside the directive
+    link: function(scope, element, attrs) {
+      scope.dialogStyle = {};
+      if (attrs.width)
+        scope.dialogStyle.width = attrs.width;
+      if (attrs.height)
+        scope.dialogStyle.height = attrs.height;
+      scope.hideModal = function() {
+        scope.show = false;
+      };
+    },
+    templateUrl: 'Dialog.html'
+  };
+});
+
